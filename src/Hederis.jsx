c96fbd5f6552347769d8eb560639584ec34e25e9
Vitālijs Korabeļņikovs <vitalijs.korabelnikovs@gmail.com>
2025-12-23 19:13:55 +0200

    hederis

---
+++ src/Hederis.jsx
@@ -0,0 +1,25 @@
+import './Hederis.css';
+import { GoogleFonts } from "next-google-fonts";
+
+function Hederis(){
+
+    return (
+        <>
+             <header>
+
+                <div className="header_left">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png" alt="logo" />
+                </div>
+                <div className="header_right">
+                    <h1>Lapas virsraksts</h1>
+                    <h2>Lapas apakšvirsraksts</h2>
+                </div>  
+             </header>
+        </>
+    )
+
+
+
+}
+
+export default Hederis
