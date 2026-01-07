e1f7c41b0c1a6a8eb4b99564820df5a62bdd2f1a
Vitālijs Korabeļņikovs <vitalijs.korabelnikovs@gmail.com>
2026-01-07 21:29:24 +0200

    grid

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,8 @@
-
-export default function Home(){
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+export default function Home() {
 
 
 
@@ -7,7 +10,22 @@ export default function Home(){
     return (
         <>
             <h1>Galvenā lapa</h1>
-        
+            <Box sx={{ flexGrow: 1 }}>
+                <Grid container spacing={2}>
+                    <Grid size={{ xs: 4, md: 2 }}>
+                        <>Saturs</>
+                    </Grid>
+                    <Grid size={{ xs: 4, md: 8 }}>
+                        <>Saturs</>
+                    </Grid>
+                    <Grid size={{ xs: 4, md: 2 }}>
+                        <>Saturs</>
+                    </Grid>
+                </Grid>
+            </Box>
+
+
+
         </>
 
     )
